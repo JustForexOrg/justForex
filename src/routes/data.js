57953d05dabc390ccfb7d19c2cc2d6f7fb5d75b7
dhee103 <dsg115@ic.ac.uk>
2017-06-13 19:15:34 +0100

    [dg] attempted to connect db to graphs

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
-var request = require('request');
+const mongojs = require('mongojs');
+var db = mongojs('mongodb://justforex:ahdgmypnd20@ds157631.mlab.com:57631/justforex', ['graphData'])
 
-router.get('*', function(req, res, next){
-    request('https://www.highcharts.com/samples/data/jsonp.php?filename=msft-c.json&callback=?', function(err, response, body) {
-      if (err || response.statusCode !== 200) {
-        return res.sendStatus(500);
-      }
-      res.json(body);
-    })
+router.get('/:id', function(req, res, next){
+    // request('https://www.doc.ic.ac.uk/~dsg115/test.php?callback=?', function(err, response, body) {
+    //   if (err || response.statusCode !== 200) {
+    //     return res.sendStatus(500);
+    //   }
+    //   // res.setHeader('content-type', 'text/javascript');
+    //   // res.contentType = "text/javascript";
+    //   res.json(body);
+    // })
+    db.graphData.findOne({_id: mongojs.ObjectsId(req.params.id)}, function(err, list) {
+      console.log(list);
+    });
 });
 
 module.exports = router;
