a0f83a96f3b7ec74dafa1b673091997758de4fd5
donamphuong <donamphuong.98@gmail.com>
2017-06-21 15:18:49 +0100

    [dg] Added placeholders to all fields. Added conditional colours to all fields. Changed split into a slider with steps of 5.

--- src/app/users/leaderboards/leaderboards.component.ts
+++ src/app/users/leaderboards/leaderboards.component.ts
@@ -3,6 +3,7 @@ import { Http } from '@angular/http';
 import { Router } from '@angular/router';
 import { MessageService } from '../chat/message.service'
 import { Message } from '../chat/message/message'
+import { FormGroup, FormBuilder, Validator} from '@angular/forms'
 
 declare var $:any;
 
@@ -130,4 +131,87 @@ export class LeaderboardsComponent {
    public roundToTwo(n: number) {
        return n.toFixed(2);
    }
+
+
+   powers = ['Really Smart', 'Super Flexible', 'Weather Changer'];
+
+  //  hero = new Hero(18, 'Dr. WhatIsHisName', this.powers[0], 'Dr. What');
+
+   submitted = false;
+
+   onSubmit() {
+     this.submitted = true;
+    //  this.hero = this.heroForm.value;
+   }
+
+   // Reset the form with a new hero AND restore 'pristine' class state
+   // by toggling 'active' flag which causes the form
+   // to be removed/re-added in a tick via NgIf
+   // TODO: Workaround until NgForm has a reset method (#6822)
+   active = true;
+   addHero() {
+    //  this.hero = new Hero(42, '', '');
+     this.buildForm();
+
+     this.active = false;
+     setTimeout(() => this.active = true, 0);
+   }
+
+  //  heroForm: FormGroup;
+
+   buildForm(): void {
+    //  this.heroForm = this.fb.group({
+    //    'name': [this.hero.name, [
+    //        Validators.required,
+    //        Validators.minLength(4),
+    //        Validators.maxLength(24),
+    //        forbiddenNameValidator(/bob/i)
+      //    ]
+      //  ],
+      //  'alterEgo': [this.hero.alterEgo],
+      //  'power':    [this.hero.power, Validators.required]
+    //  });
+
+
+    //  this.heroForm.valueChanges
+      //  .subscribe(data => this.onValueChanged(data));
+
+     this.onValueChanged(); // (re)set validation messages now
+   }
+
+
+   onValueChanged(data?: any) {
+    // //  if (!this.heroForm) { return; }
+    // //  const form = this.heroForm;
+    //
+    //  for (const field in this.formErrors) {
+    //    // clear previous error message (if any)
+    //    this.formErrors[field] = '';
+    //    const control = form.get(field);
+    //
+    //    if (control && control.dirty && !control.valid) {
+    //      const messages = this.validationMessages[field];
+    //      for (const key in control.errors) {
+    //        this.formErrors[field] += messages[key] + ' ';
+    //      }
+    //    }
+    //  }
+   }
+
+   formErrors = {
+     'name': '',
+     'power': ''
+   };
+
+   validationMessages = {
+     'name': {
+       'required':      'Name is required.',
+       'minlength':     'Name must be at least 4 characters long.',
+       'maxlength':     'Name cannot be more than 24 characters long.',
+       'forbiddenName': 'Someone named "Bob" cannot be a hero.'
+     },
+     'power': {
+       'required': 'Power is required.'
+     }
+   };
 }
